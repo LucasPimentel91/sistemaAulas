197ef085b11cf338669d246255baef1cd0316c99
Lucas <placte40@gmail.com>
2024-05-13 14:31:23 -0300

    "continuação"

--- Quadro.java
+++
@@ -1,9 +0,0 @@
-public class Quadro {
-    private String data;
-    private String horario;
-    Turma turma = new Turma();
-
-    public void registrar(String data, String horario){
-        turma.
-    }
-}
